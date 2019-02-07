e7150a6cf3175fbc8787686e3fc8a7cffd564ca3
Max Croci <mlc70@cam.ac.uk>
2019-02-07 14:16:17 +0000

    Input checks

--- tictactoe.py
+++ tictactoe.py
@@ -41,34 +41,45 @@ class Board:
 
 
     def add_move(self, player, move):
+        if move+1 not in self.__avail_positions:
+            print("***Error: this move is invalid!")
+            return False
+
         i = int(np.floor(move/3))
         j = move%3
         if player == 1:
             self.__xlocs.append([i,j])
         else:
             self.__olocs.append([i,j])
-
+        
         self.__avail_positions.remove(move+1)
         self.__update_positions()
+        
+        return True
 
     def print_avail_positions(self):
-        print("Available positions:")
-        print([pos for pos in self.__avail_positions])
+        print("Available positions: " + str([pos for pos in self.__avail_positions]))
 
 def main():
     print("Hello, welcome to tic-tac-toe!")
     board = Board()
-    #board.initialise()
     
     turn_no = 1
-    player = 1
-    
+    player = 1 
     while turn_no < 10:
-        board.print_avail_positions()
-        print("Player " + str(player) + "'s go. Please enter a number 1-9:")
-        move = int(input())
-        board.add_move(player,move-1)
-        board.print_board()
+        print("#"*15)
+        validMove = False
+        while not validMove:
+            print("Player " + str(player) + "'s go. Please enter a valid move:")
+            board.print_avail_positions()
+            move = input()
+            if move.isdigit():
+                move = int(move)
+                validMove = board.add_move(player,move-1)
+                board.print_board()
+            else:
+                print("***Error: incorrect entry. Entry must be an integer 1-9.")
+
         if turn_no%2 == 1:
             player = 2
         else:
