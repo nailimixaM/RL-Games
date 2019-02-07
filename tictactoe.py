0920424fef29145fc19349aa8a405c37047e4f4c
Max Croci <mlc70@cam.ac.uk>
2019-02-07 11:59:54 +0000

    Track available positions

--- tictactoe.py
+++ tictactoe.py
@@ -6,61 +6,68 @@ Date: 06.02.2019
 '''
 
 class Board:
-    busySquares = 0
-    x_locs = []
-    o_locs = []
-    positions = {}
+    __xlocs = []
+    __olocs = []
+    __positions = {}
+    __avail_positions = []
 
-    def initialise(self): 
+    def __init__(self):
+        self.__initialise()
+
+    def __initialise(self): 
         for i in range(9):
-            self.positions[i] = "_"
+            self.__positions[i] = "_"
+            self.__avail_positions.append(i+1)
         self.print_board()
 
-    def description(self):
-        desc_str = "This board has %i busy squares" % (self.busySquares)
-        return desc_str
-
     def print_board(self):
-        print(self.positions)
+        #print(self.__positions)
         row1 = ""
         row2 = ""
         row3 = ""
         for i in range(3):
-            row1 += self.positions[i]
-            row2 += self.positions[3+i]
-            row3 += self.positions[6+i]
+            row1 += self.__positions[i]
+            row2 += self.__positions[3+i]
+            row3 += self.__positions[6+i]
         print(row1)
         print(row2)
         print(row3)
 
-    def update_positions(self):
-        for [xi,xj] in self.x_locs:
-            self.positions[3*xi + xj] = "X"
-        for [oi,oj] in self.o_locs:
-            self.positions[3*oi + oj] = "O"
+    def __update_positions(self):
+        for [xi,xj] in self.__xlocs:
+            self.__positions[3*xi + xj] = "X"
+        for [oi,oj] in self.__olocs:
+            self.__positions[3*oi + oj] = "O"
+
 
     def add_move(self, player, move):
         i = int(np.floor(move/3))
         j = move%3
         if player == 1:
-            self.x_locs.append([i,j])
+            self.__xlocs.append([i,j])
         else:
-            self.o_locs.append([i,j])
+            self.__olocs.append([i,j])
 
-        self.update_positions()
+        self.__avail_positions.remove(move+1)
+        self.__update_positions()
+
+    def print_avail_positions(self):
+        print("Available positions:")
+        print([pos for pos in self.__avail_positions])
 
 def main():
     print("Hello, welcome to tic-tac-toe!")
     board = Board()
-    board.initialise()
+    #board.initialise()
     
     turn_no = 1
     player = 1
     
     while turn_no < 10:
-        print("Player " + str(player) + "'s go. Please enter a number 0-8:")
+        board.print_avail_positions()
+        print("Player " + str(player) + "'s go. Please enter a number 1-9:")
         move = int(input())
-        board.add_move(player,move)
+        board.add_move(player,move-1)
         board.print_board()
         if turn_no%2 == 1:
             player = 2
