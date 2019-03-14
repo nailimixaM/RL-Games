3db95e268583e3b8dafa46fdc043316e8376c28b
Max Croci <mlc70@cam.ac.uk>
2019-03-14 11:46:54 +0000

    Create ConnectX script combining C3 and C4

--- connectX.py
+++ connectX.py
@@ -9,82 +9,74 @@ Date: 21.02.2019
 '''
 
 class Board:
-    positions = {}      #Dictionary of symbols (values) at positions (keys) 1-16
+    n_rows = 0
+    n_cols = 0
+    positions = {}      #Dictionary of symbols (values) at positions (keys)
     available_moves = []#List of available moves
     visited_states = [] #List of visited states
     filled_positions =[]#List of positions filled from chosen moves
     chosen_moves = []   #List of moves that have been made
 
-    def __init__(self):
-        self.available_moves = [i + 1 for i in range(4)] #List of possible moves 1-4
+    def __init__(self,variant):
         self.positions = {}
-        for i in range(16):
-            self.positions[i+1] = "_"
         self.visited_states = []
-        self.visited_states.append("_"*16)
         self.filled_positions = []
         self.chosen_moves = []
+        if variant == 3:
+            self.n_rows = 4
+            self.n_cols = 4
+        elif variant == 4:
+            self.n_rows = 6
+            self.n_cols = 7
+
+        self.available_moves = [i + 1 for i in range(self.n_cols)]
+        for i in range(self.n_rows*self.n_cols):
+            self.positions[i+1] = "_"
+        self.visited_states.append("_"*self.n_rows*self.n_cols)
 
     def print_board(self):
-        row1 = ""
-        row2 = ""
-        row3 = ""
-        row4 = ""
-        for i in range(4):
-            row1 += self.positions[1+i]
-            row2 += self.positions[5+i]
-            row3 += self.positions[9+i]
-            row4 += self.positions[13+i]
-        print(row1)
-        print(row2)
-        print(row3)
-        print(row4)
+        for i in range(self.n_rows):
+            row = ""
+            for j in range(self.n_cols):
+                row += self.positions[1+self.n_cols*i+j]
+            print(row)
     
     def move_to_position(self,move): 
         cur_state = self.visited_states[-1]
-        if cur_state[move+11] == "_":
-            position = move+12
-        elif cur_state[move+7] == "_":
-            position = move+8
-        elif cur_state[move+3] == "_":
-            position = move+4
-        elif cur_state[move-1] == "_":
-            position = move
-
-        #print("Move " + str(move) + " equiv to position " + str(position)) 
-        self.filled_positions.append(position)
-        self.chosen_moves.append(move)
-        return position
+        for i in range(self.n_rows-1,-1,-1):
+            if cur_state[move+i*self.n_cols-1] == "_":
+                position = move+i*self.n_cols
+                self.filled_positions.append(position)
+                self.chosen_moves.append(move)
+                print("Move " + str(move) + " equiv to position " + str(position)) 
+                return position
 
     def update(self,move,symbol):
         position = self.move_to_position(move)
         self.positions[position] = symbol
+        if position <= self.n_cols:
+            self.available_moves.remove(move)
 
         state = ""
-        for i in range(16):
+        for i in range(self.n_rows*self.n_cols):
             state += self.positions[1+i]
+        
         self.visited_states.append(state)
 
     def get_next_possible_states(self, symbol):
         cur_state = self.visited_states[-1]
+        print(cur_state)
         moves = self.available_moves
         next_possible_states = {} #Dict keyed by moves, values are states
         for move in moves:
-            if cur_state[move+11] == "_":
-                new_state = cur_state[:move+11] + symbol + cur_state[move+12:]
-                next_possible_states[move] = new_state
-            elif cur_state[move+7] == "_":
-                new_state = cur_state[:move+7] + symbol + cur_state[move+8:]
-                next_possible_states[move] = new_state
-            elif cur_state[move+3] == "_":
-                new_state = cur_state[:move+3] + symbol + cur_state[move+4:]
-                next_possible_states[move] = new_state
-            elif cur_state[move-1] == "_":
-                new_state = cur_state[:move-1] + symbol + cur_state[move:]
-                next_possible_states[move] = new_state
-            #else:
-                #print("Warning: " + str(move) + " is invalid")
-
+            for i in range(self.n_rows-1,-1,-1):
+                if move not in next_possible_states:
+                    if cur_state[move+i*self.n_cols-1] == "_":
+                        new_state = cur_state[:move+i*self.n_cols-1]\
+                                + symbol\
+                                + cur_state[move+i*self.n_cols:]
+                        next_possible_states[move] = new_state
+        
         return next_possible_states
 
     def check_victory(self, symbol):
@@ -219,8 +211,40 @@ class Bot:
 
 
 def main():
-    print("Hello, welcome to connect3!")
-    
+    print("Hello, welcome to connectX!")
+
+    valid_variant = False
+    while not valid_variant:
+        print("Which variant would you like to play? Type '3' for Connect3 or '4' for Connect4:")
+        variant = input()
+        if variant is "3":
+            valid_variant = True
+        elif variant is "4":
+            valid_variant = True
+        else:
+            print("I'm sorry, " + variant + " isn't a valid option, please try again.")
+   
+    variant = int(variant)
+    quit = False
+    while not quit:
+        print("Would you like to train, test or play against a bot? Type 'r' to train, 'e' to test,'p' to play or 'q' to quit:")
+        ans = input()
+        if ans is "r":
+            train_bots(variant)
+            print('\n')
+        elif ans is "e":
+            test_bots(variant)
+            print('\n')
+        elif ans is "p":
+            play_bot(variant)
+            print('\n')
+        elif ans is "q":
+            quit = True
+            print("Goodbye!")
+        else:
+            print("I'm sorry, " + ans + " isn't a valid option, please try again.")
+
+def train_bots(variant):
     ##Train the bots over many trials##
     print("Please enter the number of trials (games) to train the bots over (10k trials takes approx 20s): ")
     MAX_NUM_TRIALS = input()
@@ -247,7 +271,7 @@ def main():
         bots = {}
         bots[1] = bot1
         bots[2] = bot2
-        board = Board()
+        board = Board(variant)
  
         ##Run the game##
         MAX_NUM_TURNS = 16
@@ -281,7 +305,29 @@ def main():
     print("X won " + str(bot1.num_wins) + " games and analysed " + str(len(bot1.V)) + " positions.")
     print("O won " + str(bot2.num_wins) + " games and analysed " + str(len(bot2.V)) + " positions.") 
     save_results(bot1,bot2,MAX_NUM_TRIALS,"trials")
+
+def test_bots(variant):
+    ##Load V from a file saved through training process##
+    print("The following saved bots exist: ")
+    num_bots = 0
+    for f in os.listdir("connect3results" + os.sep):
+        if f[0] == "V":
+            num_bots = num_bots + 1
+            print("#" + str(num_bots) + ": " + f)
     
+    if num_bots == 0:
+        print("No saved bots found! Please train a bot before testing it.")
+        return -1
+    
+    print("Type a number eg '1' to choose the bot:")
+    bot_chosen = int(input())
+    num_bots = 0
+    for f in os.listdir("connect3results" + os.sep):
+        if f[0] == "V":
+            num_bots = num_bots + 1
+            if bot_chosen == num_bots:
+                [Vx, Vo] = read_V_file(f)
+
     ##Test the bots against each other aggressively##
     print("Please enter the number of tests: ")
     MAX_NUM_TESTS = input()
@@ -293,11 +339,11 @@ def main():
     for n_trial in range(1,MAX_NUM_TESTS+1,1):
         #print("#"*15)
         if n_trial%1000 == 0:
-            print("Test: " + str(n_trial) + " of " + str(MAX_NUM_TRIALS))
+            print("Test: " + str(n_trial) + " of " + str(MAX_NUM_TESTS))
 
         if n_trial == 1:
-            bot1 = Bot("X",bot1.V,0,0,False)
-            bot2 = Bot("O",bot2.V,0,0,False)
+            bot1 = Bot("X",Vx,0,0,False)
+            bot2 = Bot("O",Vo,0,0,False)
         else:
             bot1 = Bot("X",bot1.V,bot1.num_wins,0,False) #Load key bot variables from previous trial
             bot2 = Bot("O",bot2.V,bot2.num_wins,0,False) #Load key bot variables from previous trial
@@ -305,7 +351,7 @@ def main():
         bots = {}
         bots[1] = bot1
         bots[2] = bot2
-        board = Board()
+        board = Board(variant)
  
         ##Run the game##
         MAX_NUM_TURNS = 16
@@ -338,8 +384,114 @@ def main():
     print("Testing complete!")
     print("X won " + str(bot1.num_wins) + " games and analysed " + str(len(bot1.V)) + " positions.")
     print("O won " + str(bot2.num_wins) + " games and analysed " + str(len(bot2.V)) + " positions.")
-
     save_results(bot1,bot2,MAX_NUM_TESTS,"tests")
+
+def play_bot(variant):
+    ##Load V from a file saved through training process##
+    print("The following saved bots exist: ")
+    num_bots = 0
+    for f in os.listdir("connect3results" + os.sep):
+        if f[0] == "V":
+            num_bots = num_bots + 1
+            print("#" + str(num_bots) + ": " + f)
+
+    print("Type a number eg '1' to choose the bot:")
+    bot_chosen = int(input())
+    num_bots = 0
+    for f in os.listdir("connect3results" + os.sep):
+        if f[0] == "V":
+            num_bots = num_bots + 1
+            if bot_chosen == num_bots:
+                [Vx, Vo] = read_V_file(f)
+
+    play_game = True
+    while play_game:
+        board = Board(variant)
+        print("Play as player '1' or '2'?")
+        valid_answer = False
+        while not valid_answer:
+            answer = input()
+            if answer == "1" or answer == "2":
+                player = int(answer)
+                valid_answer = True
+            else:
+                print("Error, please enter '1' or '2'")
+
+        if player == 1:
+            player_symbol = "X"
+            bot = Bot("O",Vo,0,0,False)
+        else:
+            bot = Bot("X",Vx,0,0,False)
+            player_symbol = "O"
+
+
+        ##Run the game##
+        MAX_NUM_TURNS = 42
+        turn = 1
+        victory = False
+        board.print_board()
+        while turn <= MAX_NUM_TURNS and not victory:
+            cur_player = (turn-1)%2 + 1
+            if cur_player == player:
+                print("Please make a move. Available moves:")
+                print(board.available_moves)
+                move = int(input())
+                while move not in board.available_moves:
+                    print("Error: move unavailable. Available moves:")
+                    print(board.available_moves)
+                    move = int(input())
+                board.update(move,player_symbol)
+            else:
+                n_states = board.get_next_possible_states(bot.symbol)
+                for mov, state in n_states.items():
+                    if state in bot.V:
+                        print("State " + state + " has value: " + str(bot.V[state]))
+                    else:
+                        print("State " + state + " not analysed yet.")
+
+                move = bot.get_move(board)
+                board.update(move,bot.symbol)
+
+            print("#"*15)
+            board.print_board()
+            victory = board.check_victory(bot.symbol)
+            if (victory):
+                if cur_player == player:
+                    print("Game over: you win!")
+                else:
+                    print("Game over: you lose!")
+
+            turn = turn + 1
+
+        print("Would you like to play again? (y/n)")
+        valid_answer = False
+        while not valid_answer:
+            answer = input()
+            if answer == "y":
+                play_game = True
+                valid_answer = True
+            elif answer == "n":
+                play_game = False
+                valid_answer = True
+                print("Goodbye!")
+            else:
+                print("Error, please enter 'y' or 'n':")
+
+def read_V_file(filename):
+    Vx = {}
+    Vo = {}
+    res_dir = "connect3results" + os.sep
+    f = open(res_dir + filename,'r')
+    num_lines = 0
+    for line in f:
+        num_lines = num_lines + 1
+        if num_lines%100000 == 0:
+            print("Loaded " + str(num_lines) + " lines...")
+        state, value = line.rstrip('\n').split('\t')
+        Vx[state] = float(value)
+        Vo[state] = -float(value)
+    V = [Vx, Vo]
+    return V
 
 def save_results(bot1,bot2,MAX_NUM,case):
     path = os.getcwd() + os.sep
@@ -377,6 +529,13 @@ def save_results(bot1,bot2,MAX_NUM,case):
         f.write(state[4:8] + " "*4 + "V(s) for O: " + str(round(bot2.V[state],3)) +"\n")
         f.write(state[8:12] + "\n")
         f.write(state[12:16] + "\n")
+    f.close()
+
+    filename = res_dir + "V_" + str(MAX_NUM) + "_" + case + ".txt"
+    f = open(filename,"w")
+    for state, value in bot1.V.items():
+        f.write(state + "\t" + str(value) + "\n")
+    f.close()
 
 
 
